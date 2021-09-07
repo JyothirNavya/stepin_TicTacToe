f395678ad44a46b4f1f22aff435ffbcf6eda623c
JyothirNavya <navyagannamaneni9999@gmail.com>
2021-09-07 14:52:38 +0530

    Create display_board.c

---
+++ 3_Implementation/display_board.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void display_board(char xox_changer,int* x,int* y,char chrboard[3][3])   	//prints the current state of the game board
+{ 				
+				
+			chrboard[*x][*y]=xox_changer; 
+	//printf("|_____||_____||______");
+	printf("__________________\n");	
+	for(int i=0;i<3;i++)
+		{	
+			for(int j=0;j<3;j++)
+			{
+				printf("|  %c |",chrboard[i][j]);
+			}
+			printf("\n");
+			printf("|____||____||____|\n");
+			//printf("\n");_____
+		}
+}
